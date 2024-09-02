e0a09383cf9f240ef3bbb9ee1f0d5ed04c4fa8c1
gbriellx <113640756+gbriellx@users.noreply.github.com>
2024-09-02 15:46:08 -0300

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    amount = float(request.form['amount'])
+    
+    response = requests.get('https://api.exchangerate-api.com/v4/latest/USD')
+    data = response.json()
+    rate = data['rates']['BRL']
+    
+    result = amount * rate
+    return jsonify({'result': f'{result:.2f}', 'rate': f'{rate:.4f}'})
+
+from app import app
+
+if __name__ == "__main__":
+    app.run()
